8c4107daba27e39bfffddf71017456bb3b05a956
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-23 16:51:15 +0530

    Corrected the sonar xml report for multi-module project. Finally I am able to make it success

---
+++ WebModule/src/main/java/com/amsidh/mvc/app/WebApp.java
@@ -0,0 +1,15 @@
+package com.amsidh.mvc.app;
+
+public class WebApp {
+    public static void main(String[] args) {
+
+    }
+
+    public void webAppJunit(){
+        System.out.println("WebAppJunit method WebApp's called");
+    }
+
+    public void webAppITTest(){
+        System.out.println("webAppITTest method WebApp's called");
+    }
+}
---
+++ WebModule/src/test/java/com/amsidh/mvc/app/WebAppTest.java
@@ -0,0 +1,12 @@
+package com.amsidh.mvc.app;
+
+import org.junit.Test;
+
+public class WebAppTest {
+
+
+    @Test
+    public void junitTest() {
+        new WebApp().webAppJunit();
+    }
+}
