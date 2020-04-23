d412d55ba90a5d57d4cdd1e9ca3051a4d1384d6d
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-23 08:37:56 +0530

    Initial commit

---
+++ ServiceModule/src/main/java/com/amsidh/mvc/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.amsidh.mvc.service;
+
+public class EmployeeService {
+
+    public void showJunitEmployee(){
+        System.out.println("showJunitEmployee method of com.amsidh.mvc.service.EmployeeService");
+    }
+
+    public void showIntegrationTestEmployee(){
+        System.out.println("showIntegrationTestEmployee method of com.amsidh.mvc.service.EmployeeService");
+    }
+}
---
+++ ServiceModule/src/test/java/com/amsidh/mvc/service/EmployeeServiceTest.java
@@ -0,0 +1,11 @@
+package com.amsidh.mvc.service;
+
+import org.junit.Test;
+
+public class EmployeeServiceTest {
+
+    @Test
+    public void testJunit(){
+        new EmployeeService().showJunitEmployee();
+    }
+}
